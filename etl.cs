c4a85c5ea7f29fb3ef6995bf89db6668d48c8a4b
Code Kill <steve.rives@gmail.com>
2015-11-28 23:42:16 -0600

    Improved help
    Added better comments to explain the man code file

--- etl.cs
+++ etl.cs
@@ -1,4 +1,21 @@
 ﻿// A program to import SBL GNT Morph data into a postgres table in a local databae.
+// Run this program in one of these modes:
+// 
+//    Mode 1: Stats Generation Mode
+//
+//             ETL -root
+//
+//            This will generate statistics on all the words (their root forms) 
+//            as used in each verse of the bible.
+//
+//    Mode 2: Import file mode
+//
+//             ETL some_greek_morph_filename.txt book_name short_name
+//  
+//            This mode will import one file into the postgres database into Database=Bible table=Greek
+//
+//  
+//
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,14 +42,21 @@ namespace etl
                 Console.WriteLine("       Greek NT Database Population tool.");
                 Console.WriteLine("       Populates a Postgres database with the SBL GNT Morphological text files.");
                 Console.WriteLine("Two modes:");
-                Console.WriteLine("     Mode 1: Update the populated database (this is done with the -root options)");
-                Console.WriteLine("         This mode will generate a 'rives-number' for each root word in the NT");
-                Console.WriteLine("         It will write the rives-number into each row of the Greek table.");
+                Console.WriteLine("     Mode 1: Update the populated database (this is done with the -root option)");
+                Console.WriteLine("         This mode will generate a unique number for each root word in the NT");
+                Console.WriteLine("         It will write this number into each row of the Greek table.");
                 Console.WriteLine("         This assumes that you have already run this program to populate the database");
+                Console.WriteLine("         The benefit of doing this is speeding up searching.");
+                Console.WriteLine("         You will be able to write software to search on number instead of strings.");
                 Console.WriteLine("     Mode 2: Populate the database");
-                Console.WriteLine("       Will import SBL GNT Morph data into postgre database = Bible, table = Greek");
-                Console.WriteLine("       For example:");
+                Console.WriteLine("         This will import SBL GNT Morph data into postgre database = Bible, table = Greek");
+                Console.WriteLine("         For example:");
                 Console.WriteLine("                        ETL 61-Mt-morphgnt.txt Matthew Mt");
+                Console.WriteLine();
+                Console.WriteLine("For the format of the talbe we modify, see the SQL script, createTableGreek.sql");
+                Console.WriteLine("For this and more, go to https://github.com/srives/ETL");
+                Console.WriteLine();
+                Console.WriteLine("Stephen S. Rives, 2015");
             }           
         }
 
