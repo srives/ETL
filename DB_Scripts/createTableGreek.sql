e712bb3a8eceaf415deb9961a6b75d3808c29441
Sam Kovacic <sam.kovacic@essentialstages.com>
2019-12-29 16:27:32 -0600

    fixed database table encoding

--- DB_Scripts/createTableGreek.sql
+++ DB_Scripts/createTableGreek.sql
@@ -4,45 +4,45 @@
 
 CREATE TABLE Greek
 (
-  book_name character varying(30), -- Long name of book
-  short_name character varying(5),
-  canon_order smallint, -- Protestant Canonical sequence number....
-  chapter smallint,
-  verse smallint,
-  sentence_position smallint, -- Order of this word in this sentence
-  book_position integer, -- Position of this word in this book. This way we can measure distance between words. Each book is like its own number line.
-  adjective bit,
-  conjunction bit,
-  adverb bit,
-  interjection bit,
-  noun bit,
-  preposition bit,
-  article bit,
-  demons_pronoun bit, -- demonstrative pronoun
-  indef_pronoun bit, -- interrogative/indefinite pronoun
-  person_pronoun bit, -- personal pronoun
-  relative_pronoun bit, -- relative pronoun
-  verb bit,
-  particle bit,
-  person smallint, -- person (1=1st, 2=2nd, 3=3rd)
-  tense character varying(15), -- tense (P=present, I=imperfect, F=future, A=aorist, X=perfect, Y=pluperfect)
-  voice character varying(15), -- voice (A=active, M=middle, P=passive)
-  mood character varying(15), -- mood (I=indicative, D=imperative, S=subjunctive, O=optative, N=infinitive, P=participle)
-  noun_case character varying(15), -- case (N=nominative, G=genitive, D=dative, A=accusative)
-  sing_or_plural character(1), -- S = singular...
-  gender character(1), -- M=masculine, F=feminine, N=neuter
-  degree character(1), -- degree (C=comparative, S=superlative)
-  word_with_punct character varying(40), -- The word, with sentence markers like question marks, etc.
-  word_without_punct character varying(40), -- The word without any question marks or such on the end.
-  word character varying(40), -- The with normalzed accents and such. Good for searching on exact hits.
-  root character varying(40), -- Root word
-  strongs_num integer,
-  family_num integer, -- Each word can belong to a family, and that family is a number. This is for speed searching on relationships. This number will be based on how words group and relate.
-  root_num integer, -- Every unique Greek root gets a number, to speed searching
-  root_freq_nt integer, -- How often does the root word appear in the Greek NT
-  word_freq_nt integer, -- How often this word appears as is in whole NT
-  root_freq_book integer, -- How often this root word appears in this book
-  word_freq_book integer -- How often this word as this word like this word in this form appears in this book
+  book_name nvarchar(30) not null, -- Long name of book
+  short_name nvarchar(10) not null,
+  canon_order smallint not null default -1, -- Protestant Canonical sequence number....
+  chapter smallint not null default -1,
+  verse smallint not null default -1,
+  sentence_position smallint not null default -1, -- Order of this word in this sentence
+  book_position int not null default -1, -- Position of this word in this book. This way we can measure distance between words. Each book is like its own number line.
+  adjective bit not null default 0,
+  conjunction bit not null default 0,
+  adverb bit not null default 0,
+  interjection bit not null default 0,
+  noun bit not null default 0,
+  preposition bit not null default 0,
+  article bit not null default 0,
+  demons_pronoun bit not null default 0, -- demonstrative pronoun
+  indef_pronoun bit not null default 0, -- interrogative/indefinite pronoun
+  person_pronoun bit not null default 0, -- personal pronoun
+  relative_pronoun bit not null default 0, -- relative pronoun
+  verb bit not null default 0,
+  particle bit not null default 0,
+  person smallint not null default -1, -- person (1=1st, 2=2nd, 3=3rd)
+  tense nvarchar(30), -- tense (P=present, I=imperfect, F=future, A=aorist, X=perfect, Y=pluperfect)
+  voice nvarchar(30), -- voice (A=active, M=middle, P=passive)
+  mood nvarchar(30), -- mood (I=indicative, D=imperative, S=subjunctive, O=optative, N=infinitive, P=participle)
+  noun_case nvarchar(30), -- case (N=nominative, G=genitive, D=dative, A=accusative)
+  sing_or_plural char(1), -- S = singular...
+  gender char(1), -- M=masculine, F=feminine, N=neuter
+  degree char(1), -- degree (C=comparative, S=superlative)
+  word_with_punct nvarchar(80), -- The word, with sentence markers like question marks, etc.
+  word_without_punct nvarchar(80), -- The word without any question marks or such on the end.
+  word nvarchar(80), -- The with normalzed accents and such. Good for searching on exact hits.
+  root nvarchar(80), -- Root word
+  strongs_num int not null default -1,
+  family_num int not null default -1, -- Each word can belong to a family, and that family is a number. This is for speed searching on relationships. This number will be based on how words group and relate.
+  root_num int not null default -1, -- Every unique Greek root gets a number, to speed searching
+  root_freq_nt int not null default -1, -- How often does the root word appear in the Greek NT
+  word_freq_nt int not null default -1, -- How often this word appears as is in whole NT
+  root_freq_book int not null default -1, -- How often this root word appears in this book
+  word_freq_book int not null default -1 -- How often this word as this word like this word in this form appears in this book
 )
 
 ALTER TABLE Greek
